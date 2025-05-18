d517e9e0fba765c5aed733c5ec1fe3540a2a566b
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2025-05-18 18:32:39 -0300

    wip: Monstros_Saltitantes

--- Saltitantes_Java/src/Screen.java
+++ Saltitantes_Java/src/Screen.java
@@ -7,92 +7,94 @@ public class Screen {
     private static int width = 720;
     private static int height = 480;
 
-    private static JFrame frame;
-    private static JPanel panel;
+    private JFrame frame;
+    private BallPanel ballPanel;
 
-    private static ArrayList<PanelBall> list;
     public void initScreen() {
-        list = new ArrayList<>();
         frame = new JFrame("Saltitantes");
         frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        addPanel();
+
+        ballPanel = new BallPanel(width, height);
+        ballPanel.setLayout(null);
+        frame.add(ballPanel);
+
         addButton();
 
         frame.setVisible(true);
+        ballPanel.startTimer();
     }
 
-    private void addPanel(){
-        panel = new JPanel();
-        panel.setLayout(null);
-        panel.setBackground(Color.BLACK);
-        frame.add(panel);
-    }
     private void addButton() {
         JButton button = new JButton("ADD BALL");
         int buttonWidth = 100;
         int buttonHeight = 50;
         button.setBounds(width - buttonWidth - 20, 10, buttonWidth, buttonHeight);
-        panel.add(button);
-        panel.setComponentZOrder(button, 0);
-        button.addActionListener( e -> addBallInRandomPos());
+        ballPanel.add(button);
+        ballPanel.setComponentZOrder(button, 0);
+        button.addActionListener(e -> addBallInRandomPos());
     }
 
-    private void addBallInRandomPos(){
+    private void addBallInRandomPos() {
         Random rand = new Random();
-        int randomNumber = rand.nextInt(width);
-        addBall(randomNumber);
+        int randomX = rand.nextInt(width - BallPanel.BALL_SIZE);
+        ballPanel.addBall(randomX);
     }
-    private void addBall(int posX){
-        PanelBall ball = new PanelBall(width, height, posX);
-        ball.setBounds(0, 0, width, height);
-        ball.setOpaque(false);
-        ball.startTimer();
-        panel.add(ball);
-        list.add(ball);
-    }
-
 }
 
-class PanelBall extends JPanel{
-    private int x;
-    private int y;
-    private int spd = 0;
+class BallPanel extends JPanel {
+    static final int BALL_SIZE = 50;
+    private final int groundY;
+
+    private final ArrayList<Ball> balls = new ArrayList<>();
+    private Timer timer;
+
     private final int grav = 1;
     private final int jumpForce = -15;
-    private final int groundY;
-    private final int ballSize = 50;
-    private Timer timer;
-    PanelBall(int Widht, int Height, int posX) {
 
-        this.groundY = Height - ballSize - 40;
-        this.y = this.groundY;
-        if(posX >= 0 && posX <= Widht - ballSize)
-            this.x = posX;
+    BallPanel(int width, int height) {
+        setBackground(Color.BLACK);
+        setPreferredSize(new Dimension(width, height));
+        groundY = height - BALL_SIZE - 40;
     }
 
-    public void startTimer(){
+    public void addBall(int posX) {
+        balls.add(new Ball(posX, groundY));
+    }
+
+    public void startTimer() {
         timer = new Timer(16, e -> update());
         timer.start();
     }
 
-    public void update(){
-        this.spd += this.grav;
-        this.y += this.spd;
-
-        if (y >= groundY){
-            y = groundY;
-            spd = jumpForce;
+    private void update() {
+        for (Ball ball : balls) {
+            ball.spdY += grav;
+            ball.y += ball.spdY;
+            if (ball.y >= groundY) {
+                ball.y = groundY;
+                ball.spdY = jumpForce;
+            }
         }
-
         repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         g.setColor(Color.BLUE);
-        g.fillOval(x, y, ballSize, ballSize);
+        for (Ball ball : balls) {
+            g.fillOval(ball.x, ball.y, BALL_SIZE, BALL_SIZE);
+        }
+    }
+}
+
+class Ball {
+    int x, y;
+    int spdY = 0;
+
+    Ball(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
