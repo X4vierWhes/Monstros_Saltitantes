bc4da31042017224dad246a523a06098741a8fc7
X4vierWhes <xavierwhesley@gmail.com>
2025-07-21 23:59:41 -0300

    wip: TEST

--- Saltitantes_Java/src/main/java/org/tests/CreaturesPanelPropertyTest.java
+++ Saltitantes_Java/src/main/java/org/tests/CreaturesPanelPropertyTest.java
@@ -86,7 +86,7 @@ public class CreaturesPanelPropertyTest {
             panel.addCreature(pos);
         }
         // Simula alguns passos de física
-        for(int i=0; i<10; i++) {
+        for(int i=0; i<100; i++) {
             panel.phisycsUpdate();
         }
 
